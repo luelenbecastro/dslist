75e0225ba30e5c6bf60ed3ed0ef8433bf549d272
Luelen Becastro <thomazluelen@gmail.com>
2023-05-14 16:10:02 -0300

    GET lists

--- src/main/java/com/example/dslist/services/GameService.java
+++ src/main/java/com/example/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.dslist.dto.GameDTO;
 import com.example.dslist.dto.GameMinDTO;
 import com.example.dslist.entities.Game;
+import com.example.dslist.projections.GameMinProjection;
 import com.example.dslist.repositories.GameRepository;
 
 @Service
@@ -30,4 +31,11 @@ public class GameService {
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
-}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	}
+	
