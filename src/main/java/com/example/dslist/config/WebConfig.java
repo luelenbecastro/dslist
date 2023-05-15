41d32046c3f0021f3560ce797da2f8307ef6718d
Luelen Becastro <thomazluelen@gmail.com>
2023-05-14 23:19:55 -0300

    Move games on list

--- src/main/java/com/example/dslist/config/WebConfig.java
+++ src/main/java/com/example/dslist/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
 			}
-		};
+		}; 
 	}
 
 }
